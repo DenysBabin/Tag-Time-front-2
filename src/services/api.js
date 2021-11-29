19c026b43bbed27a40984f3f45562f31811868ef
DenBa <d.babin2173@gmail.com>
2021-11-29 17:33:09 +0100

    port back 4000

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios'
 export default () => {
   return axios.create({
-    baseURL: 'http://denbabinis.com:6000'
+    baseURL: 'http://denbabinis.com:4000'
+    // headers: {
+    //   'Content-Type': 'text/plain',
+    //   'Sec-Fetch-Mode': 'no-cors'
+    // }
   })
 }
